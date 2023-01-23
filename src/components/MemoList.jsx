e92d8f6be844db3f0b019ad5e2dab671afce9796
Neil <mitsuru.jack@gmail.com>
2023-01-23 17:53:50 +0900

    data fetched from firestore and passed to FlatList

--- src/components/MemoList.jsx
+++ src/components/MemoList.jsx
@@ -1,66 +1,69 @@
 import React from 'react';
 import {
-    StyleSheet, Text, View, TouchableOpacity, Alert,
+    StyleSheet, Text, View, TouchableOpacity, Alert, FlatList,
 } from 'react-native';
 // navigationが設定されていないcomponentsで使えるようにする設定。
 import { useNavigation } from '@react-navigation/native';
+// memosはobjectの配列が入るのでarrayOfとshape、bodyTextは文字列なのでstring、updatedAtはデータ型なのでinstanceOf
+import {
+    shape, string, instanceOf, arrayOf,
+} from 'prop-types';
 
 import Icon from './Icon';
 
-export default function MemoList() {
+export default function MemoList(props) {
+    const { memos } = props;
     const navigation = useNavigation();
+
+    function renderItem({ item }) {
+        return (
+            <TouchableOpacity
+                // リストが重複しないように、idをkeyにする必要がある。
+                // ただし今回は下のkeyExtractorで指定しているので不要。
+                // key={item.id}
+                style={styles.memoListItem}
+                onPress={() => { navigation.navigate('MemoDetail'); }}
+            >
+                <View>
+                    <Text style={styles.memoListItemTitle} numberOfLines={1}>{item.bodyText}</Text>
+                    <Text style={styles.memoListItemDate}>{String(item.updatedAt)}</Text>
+                </View>
+                <TouchableOpacity
+                    style={styles.memoDelete}
+                    onPress={() => { Alert.alert('Are you sure?'); }}
+                >
+                    <Icon name="delete" size={24} color="#B0B0B0" />
+                </TouchableOpacity>
+            </TouchableOpacity>
+        );
+    }
+
     return (
-        <View>
-            <TouchableOpacity
-                style={styles.memoListItem}
-                onPress={() => { navigation.navigate('MemoDetail'); }}
-            >
-                <View>
-                    <Text style={styles.memoListItemTitle}>買い物リスト</Text>
-                    <Text style={styles.memoListItemDate}>2023年1月16日 10:00</Text>
-                </View>
-                <TouchableOpacity
-                    style={styles.memoDelete}
-                    onPress={() => { Alert.alert('Are you sure?'); }}
-                >
-                    <Icon name="delete" size={24} color="#B0B0B0" />
-                </TouchableOpacity>
-            </TouchableOpacity>
-            <TouchableOpacity
-                style={styles.memoListItem}
-                onPress={() => { navigation.navigate('MemoDetail'); }}
-            >
-                <View>
-                    <Text style={styles.memoListItemTitle}>買い物リスト</Text>
-                    <Text style={styles.memoListItemDate}>2023年1月16日 10:00</Text>
-                </View>
-                <TouchableOpacity
-                    style={styles.memoDelete}
-                    onPress={() => { Alert.alert('Are you sure?'); }}
-                >
-                    <Icon name="delete" size={24} color="#B0B0B0" />
-                </TouchableOpacity>
-            </TouchableOpacity>
-            <TouchableOpacity
-                style={styles.memoListItem}
-                onPress={() => { navigation.navigate('MemoDetail'); }}
-            >
-                <View>
-                    <Text style={styles.memoListItemTitle}>買い物リスト</Text>
-                    <Text style={styles.memoListItemDate}>2023年1月16日 10:00</Text>
-                </View>
-                <TouchableOpacity
-                    style={styles.memoDelete}
-                    onPress={() => { Alert.alert('Are you sure?'); }}
-                >
-                    <Icon name="delete" size={24} color="#B0B0B0" />
-                </TouchableOpacity>
-            </TouchableOpacity>
+        <View style={styles.container}>
+            {/* FlatListを使うことで画面に表示される項目のみの表示に限定できる。
+                使用しないと全てのデータが常にスクロール外にも表示されていることになり
+                データが多い場合パフォーマンスが落ちてしまう。 */}
+            <FlatList
+                data={memos}
+                renderItem={renderItem}
+                keyExtractor={(item) => item.id}
+            />
         </View>
     );
 }
 
+MemoList.propTypes = {
+    memos: arrayOf(shape({
+        id: string,
+        bodyText: string,
+        updatedAt: instanceOf(Date),
+    })).isRequired,
+};
+
 const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+    },
     memoListItem: {
         backgroundColor: '#ffffff',
         flexDirection: 'row',
--- src/screens/MemoListScreen.jsx
+++ src/screens/MemoListScreen.jsx
@@ -1,5 +1,6 @@
-import React, { useEffect } from 'react';
-import { View, StyleSheet } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { View, StyleSheet, Alert } from 'react-native';
+import firebase from 'firebase';
 
 import MemoList from '../components/MemoList';
 import CircleButton from '../components/CircleButton';
@@ -7,15 +8,51 @@ import LogOutButton from '../components/LogOutButton';
 
 export default function MemoListScreen(props) {
     const { navigation } = props;
+    const [memos, setMemos] = useState([]);
     useEffect(() => {
         // navigationのAppBarを使っているので、navigationを編集していく。
         navigation.setOptions({
             headerRight: () => <LogOutButton />,
         });
     }, []);
+
+    useEffect(() => {
+        const db = firebase.firestore();
+        const { currentUser } = firebase.auth();
+        // currentUserはログアウトされるとエラーになるためifの処理にする。
+        // unsubscribeをif内に入れたため、if外に定義する必要がある。
+        // letで定義し、再代入可能な状態にする。
+        // すでに定義したためif内でconstは不要になる。
+        let unsubscribe = () => {};
+        if (currentUser) {
+            const ref = db.collection(`users/${currentUser.uid}/memos`).orderBy('updatedAt', 'desc');
+            unsubscribe = ref.onSnapshot((snapshot) => {
+                const userMemos = [];
+                // メモの中にあるそれぞれのリストを参照できる。
+                // arrowを並列に並べる時、前者が.then、後者が.catchと同じ扱われ方になる。
+                snapshot.forEach((doc) => {
+                    console.log(doc.id, doc.data());
+                    const data = doc.data();
+                    // pushでuserMemosの配列に情報を追加できる。
+                    userMemos.push({
+                        id: doc.id,
+                        bodyText: data.bodyText,
+                        updatedAt: data.updatedAt.toDate(),
+                    });
+                });
+                // 一時的に設定したuserMemosをsetMemosに入れる。
+                setMemos(userMemos);
+            }, (error) => {
+                console.log(error);
+                Alert.alert('データの読み込みに失敗しました。');
+            });
+        }
+        return unsubscribe;
+    }, []);
+
     return (
         <View style={styles.container}>
-            <MemoList />
+            <MemoList memos={memos} />
             <CircleButton
                 name="plus"
                 onPress={() => { navigation.navigate('MemoCreate'); }}
